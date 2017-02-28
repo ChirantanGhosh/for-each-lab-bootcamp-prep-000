34c5b6921256fd99257974369195f936835d4745
Chirantan Ghosh <chrn.ghosh@gmail.com>
2017-02-28 22:53:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(arr) {
+  arr.forEach((elem, index) => {
+    console.log(`${index}: ${elem}`)
+  })
+}
+
+function iterate(callback) {
+  var arr = [1,2,3];
+  arr.forEach(callback); return arr;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+  return array
+}
+
